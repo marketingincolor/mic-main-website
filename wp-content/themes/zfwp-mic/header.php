44041075d1fcc62f92f7f7612503c553a45a0a34
Adam Doe <ADoe@mic.lan>
2016-06-28 09:20:18 -0400

    Add hreflang tag for SEO

--- wp-content/themes/zfwp-mic/header.php
+++ wp-content/themes/zfwp-mic/header.php
@@ -36,6 +36,7 @@ $header_class = 'header-base';
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title><?php bloginfo('name'); ?> </title>
+	<link rel="alternate" href="http://www.marketingincolor.com" hreflang="en-us" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans|Arvo|Lato' rel='stylesheet' type='text/css'>
