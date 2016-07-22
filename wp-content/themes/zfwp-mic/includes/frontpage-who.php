b098f6c4731cbb7fc64b021bbba512df6f952acf
Adam Doe <adoe@marketingincolor.com>
2016-07-22 08:35:38 -0400

    Added alt attributes to crayons

--- wp-content/themes/zfwp-mic/includes/frontpage-who.php
+++ wp-content/themes/zfwp-mic/includes/frontpage-who.php
@@ -11,7 +11,7 @@
     <ul>
         <li>
             <input type="radio" id="vert-1" name="vert-accordion" checked="checked" />
-            <label for="vert-1"><img src="<?php echo get_template_directory_uri(); ?>/img/mic-grfx-expd-purp.png"></label>
+            <label for="vert-1"><img src="<?php echo get_template_directory_uri(); ?>/img/mic-grfx-expd-purp.png" alt="Vertical purple crayon that's labled blog"></label>
             <div class="acc-content bgnd-purple">
 	            <div class="acc-content-shadow"></div>
 
@@ -32,7 +32,7 @@
         </li>
         <li>
             <input type="radio" id="vert-2" name="vert-accordion" />
-            <label for="vert-2"><img src="<?php echo get_template_directory_uri(); ?>/img/mic-grfx-expd-orng.png"></label>
+            <label for="vert-2"><img src="<?php echo get_template_directory_uri(); ?>/img/mic-grfx-expd-orng.png" alt="Vertical orange crayon that's labled team"></label>
             <div class="acc-content bgnd-orange">
                 <div class="acc-content-shadow"></div>
 
@@ -59,7 +59,7 @@
         </li>
         <li>
             <input type="radio" id="vert-3" name="vert-accordion" />
-            <label for="vert-3"><img src="<?php echo get_template_directory_uri(); ?>/img/mic-grfx-expd-gren.png"></label>
+            <label for="vert-3"><img src="<?php echo get_template_directory_uri(); ?>/img/mic-grfx-expd-gren.png" alt="Vertical green crayon that's labled our true colors"></label>
             <div class="acc-content bgnd-green">
                 <div class="acc-content-shadow"></div>
                 <?php
@@ -81,7 +81,7 @@
         </li>
         <li>
             <input type="radio" id="vert-4" name="vert-accordion" />
-            <label for="vert-4"><img src="<?php echo get_template_directory_uri(); ?>/img/mic-grfx-expd-slvr.png"></label>
+            <label for="vert-4"><img src="<?php echo get_template_directory_uri(); ?>/img/mic-grfx-expd-slvr.png" alt="Vertical gray crayon that's labled downtown"></label>
             <div class="acc-content bgnd-silver">
                 <div class="acc-content-shadow"></div>
 
