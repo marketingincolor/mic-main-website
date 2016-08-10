6a68866a5a7b87d6afe0d8daefcd76d5884aeabf
Adam Doe <adoe@marketingincolor.com>
2016-08-10 12:12:44 -0400

    Additional GTM/Custom Dimension Stuff -Removed gtm.php -Moved GTM script to header.php

--- wp-content/themes/zfwp-mic/header.php
+++ wp-content/themes/zfwp-mic/header.php
@@ -76,8 +76,15 @@ $header_class = 'header-base';
 
 <body <?php body_class(); ?>>
 
-<?php include ('gtm.php'); ?>
-
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TLNW5X"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TLNW5X');</script>
+<!-- End Google Tag Manager -->
 	
 		<div class="<?php echo $header_class; ?> NOTorange-bg NOTcontain-to-grid NOTrow NOTlarge-collapse hide-for-small-down">
 	        <header id="masthead" class="site-header" role="banner">
